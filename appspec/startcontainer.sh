9c10320269a06bc10cb2ae4e03dd50b5ba3352b6
TStickney <tstick56@hotmail.com>
2017-11-01 11:26:49 -0700

    Update startcontainer.sh

--- appspec/startcontainer.sh
+++ appspec/startcontainer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 $(aws ecr get-login --region us-west-2)
-docker pull 203417976784.dkr.ecr.us-west-2.amazonaws.com/comp4913/pywebapp:latest || {
+docker pull 993884755824.dkr.ecr.us-west-2.amazonaws.com/tstickney/pywebapp:latest || {
     echo "ERROR: docker pull failed. Sleeping for 10 minutes to allow investigation..."
     sleep 600
     exit 1
