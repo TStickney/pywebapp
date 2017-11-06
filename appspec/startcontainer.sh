fe9273c87e755408e40c62b7483c6ab606da2494
TStickney <tstick56@hotmail.com>
2017-11-05 22:00:45 -0800

    Added no include email

--- appspec/startcontainer.sh
+++ appspec/startcontainer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-$(aws ecr get-login --region us-west-2)
+$(aws ecr get-login --region us-west-2 --no-include-email)
 docker pull 993884755824.dkr.ecr.us-west-2.amazonaws.com/tstickney/pywebapp:latest || {
     echo "ERROR: docker pull failed. Sleeping for 10 minutes to allow investigation..."
     sleep 60
